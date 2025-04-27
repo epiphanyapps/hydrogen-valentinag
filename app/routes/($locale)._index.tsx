1f149019bd9664ba5a766cd62df2821e0aa93809
Walter Vargas-Pena <walter.vargaspena@gmail.com>
2025-04-26 23:13:34 -0400

    Add logging for collections in loadCriticalData function

--- app/routes/($locale)._index.tsx
+++ app/routes/($locale)._index.tsx
@@ -17,6 +17,7 @@ export async function loader(args: LoaderFunctionArgs) {
 
   // Await the critical data required to render initial state of the page
   const criticalData = await loadCriticalData(args);
+
   console.log('criticalData', criticalData);
   return {...deferredData, ...criticalData};
 }
@@ -27,11 +28,11 @@ export async function loader(args: LoaderFunctionArgs) {
  */
 async function loadCriticalData({context}: LoaderFunctionArgs) {
   const [{collections}] = await Promise.all([
-    console.log('context.storefront', context.storefront),
     context.storefront.query(FEATURED_COLLECTION_QUERY),
     // Add other queries here, so that they are loaded in parallel
   ]);
 
+  console.log('collections', collections);
   return {
     featuredCollection: collections.nodes[0],
   };
